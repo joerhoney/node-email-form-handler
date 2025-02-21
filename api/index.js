e0475b20d8b3d0937cdeb21fd0441d9dc0cd99da
joerhoney <6787748+joerhoney@users.noreply.github.com>
2025-02-21 12:58:01 -0800

    Optimized for email service.

--- api/index.js
+++ api/index.js
@@ -12,8 +12,9 @@ export default async function handler(req, res) {
   }
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.your-email-provider.com",
-    port: 587,
+    host: process.env.SMTP_HOST,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
